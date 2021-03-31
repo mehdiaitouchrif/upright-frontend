b720ba87fb447ee5bfe943b19ea284be3a8a1e5f
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-03-31 23:04:19 +0100

    Create container for pages

---
+++ src/components/AppContainer/index.jsx
@@ -0,0 +1,43 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+import { logout, getCurrentUser } from '../../actions/authActions'
+import Container from '../UI/Container'
+import Aside from '../Aside'
+import Navigation from '../Navigation'
+import './AppContainer.scss'
+
+function AppContainer({ children }) {
+	const dispatch = useDispatch()
+	const history = useHistory()
+
+	const userLogin = useSelector((state) => state.userLogin)
+	const { userInfo } = userLogin
+
+	const currentUser = useSelector((state) => state.currentUser)
+	const { user } = currentUser
+
+	function logoutHandler() {
+		dispatch(logout())
+		window.location.reload()
+	}
+
+	useEffect(() => {
+		if (!user) {
+			dispatch(getCurrentUser())
+		}
+
+		if (!userInfo) {
+			history.push('/login')
+		}
+	}, [dispatch, history, user, userInfo])
+	return (
+		<Container className='app-container'>
+			<Navigation user={user} logout={logoutHandler} />
+			{children}
+			<Aside user={user} />
+		</Container>
+	)
+}
+
+export default AppContainer
