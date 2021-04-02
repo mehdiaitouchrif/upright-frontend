7d36ad407ef11c8dac4990d301e246cc5b5221aa
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-04-02 14:01:28 +0100

    Update edit

--- src/components/Posts/Edit/index.jsx
+++ src/components/Posts/Edit/index.jsx
@@ -3,13 +3,13 @@ import { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { updatePost } from '../../../actions/postActions'
 import Modal from '../../UI/Modal'
-import Container from '../../UI/Container'
-import Card from '../../UI/Card'
-import Flex from '../../UI/Flex'
+// import Container from '../../UI/Container'
+// import Card from '../../UI/Card'
+// import Flex from '../../UI/Flex'
 import TextArea from '../../UI/FormComponents/TextArea'
 import Button from '../../UI/Button'
 
-function Edit({ post, showModal }) {
+function Edit({ post, showModal, title, customSize }) {
 	const [text, setText] = useState('')
 	const [image, setImage] = useState('')
 
@@ -27,37 +27,27 @@ function Edit({ post, showModal }) {
 	}, [post])
 
 	return (
-		<Modal>
-			<Container size='md'>
-				<Card>
-					<Flex justify='space-between' align='center'>
-						<h3 className='tertiary-heading'>Edit your post</h3>
-						<p className='modal__close' onClick={showModal}>
-							&times;
-						</p>
-					</Flex>
-
-					<TextArea
-						name='text'
-						placeholder='Say something!'
-						onChange={(e) => setText(e.target.value)}
-						value={text}
-					/>
-					{image && (
-						<div className='edit__image'>
-							<img src={image} alt={image} />
-						</div>
-					)}
-					<Button
-						onClick={updateHandler}
-						type='submit'
-						bg='blue'
-						className='rounded'
-					>
-						Update
-					</Button>
-				</Card>
-			</Container>
+		<Modal title={title} showModal={showModal} customSize={customSize}>
+			<TextArea
+				name='text'
+				className='edit__input'
+				placeholder='Say something!'
+				onChange={(e) => setText(e.target.value)}
+				value={text}
+			/>
+			{image && (
+				<div className='edit__image'>
+					<img src={image} alt={image} />
+				</div>
+			)}
+			<Button
+				onClick={updateHandler}
+				type='submit'
+				bg='blue'
+				className='rounded'
+			>
+				Update
+			</Button>
 		</Modal>
 	)
 }
