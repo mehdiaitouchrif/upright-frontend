2ea6baaec20fc024f804955a3e4fbc61a6f83fc0
mehdiaitouchrif <mehdi.aitouchrif1@gmail.com>
2021-04-02 11:48:22 +0100

    Update Alert component

--- src/components/UI/Alert/index.jsx
+++ src/components/UI/Alert/index.jsx
@@ -1,7 +1,28 @@
+import { useRef } from 'react'
+import Flex from '../Flex'
 import './Alert.scss'
 
 function Alert({ children, bg, className }) {
-	return <div className={`alert alert-${bg} ${className}`}>{children}</div>
+	const alertContainer = useRef(null)
+
+	function clearAlert() {
+		alertContainer.current.style.display = 'none'
+	}
+
+	return (
+		<div ref={alertContainer} className='alert'>
+			<Flex
+				align='center'
+				justify='space-between'
+				className={`alert alert-${bg} ${className}`}
+			>
+				<p>{children}</p>
+				<p className='alert__close' onClick={clearAlert}>
+					&times;
+				</p>
+			</Flex>
+		</div>
+	)
 }
 
 export default Alert
